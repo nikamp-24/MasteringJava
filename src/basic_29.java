b3551453bb8c9a49ff7339041289ef00b288568c
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-01-24 22:16:35 +0530

    init commit

---
+++ src/basic_29.java
@@ -0,0 +1,10 @@
+public class basic_29 {
+    public static void main(String[] args) {
+        System.out.println("Twinkle, twinkle, little star,\n" +
+                "\tHow I wonder what you are!\n" +
+                "\t\tUp above the world so high,\n" +
+                "\t\tLike a diamond in the sky.\n" +
+                "Twinkle, twinkle, little star,\n" +
+                "\tHow I wonder what you are\n");
+    }
+}
