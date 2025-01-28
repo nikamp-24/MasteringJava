c0c1044876220f09b377bd56b98b5fbcd1506c32
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-01-28 12:01:40 +0530

    init commit

--- src/Home/Java_basics/basic_63.java
+++ src/Home/Java_basics/basic_63.java
@@ -1,7 +1,17 @@
 package Home.Java_basics;
 
+import java.util.Arrays;
+
 public class basic_63 {
     public static void main(String[] args) {
+        int[] arr = {20, 30, 40} ;
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        int x = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = x ;
+
+        System.out.println("New array after swapping the first and last elements: " + Arrays.toString(arr));
 
     }
 }
--- src/Home/Java_basics/basic_65.java
+++ src/Home/Java_basics/basic_65.java
@@ -2,6 +2,14 @@ package Home.Java_basics;
 
 public class basic_65 {
     public static void main(String[] args) {
+int[] arr1 = {1, 3, -5, 4} ;
+int[] arr2 = {1, 4, -5, -2} ;
+
+for (int i = 0; i <= arr1.length -1 ; i++){
+    int num1 = arr1[i];
+    int num2 = arr2[i];
+    System.out.print(Integer.toString(num1 * num2) + " ");
+}
 
     }
 }
