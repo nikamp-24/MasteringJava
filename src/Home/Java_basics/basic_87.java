d4a87ff61178e32042265b63b90b1e620e1e413d
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-03-15 17:54:41 +0530

    init commit

--- src/Home/Java_basics/basic_87.java
+++ src/Home/Java_basics/basic_87.java
@@ -1,4 +1,23 @@
 package Home.Java_basics;
 
+import java.util.Arrays;
+
 public class basic_87 {
+
+    public static int[] leftShift(int[] arr) {
+        int first = arr[0];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = first;
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 5};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        int[] shiftedArray = leftShift(arr);
+        System.out.println("Left Shifted Array: " + Arrays.toString(shiftedArray));
+    }
 }
--- src/Home/Java_basics/basic_88.java
+++ src/Home/Java_basics/basic_88.java
@@ -1,4 +1,21 @@
 package Home.Java_basics;
 
+import java.util.Arrays;
+
 public class basic_88 {
+    public static boolean threeAdjInc(int[] arr) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] + 1 == arr[i + 1] && arr[i + 1] + 1 == arr[i + 2]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 6, 8, 9};
+        System.out.println("Original Array:" + Arrays.toString(arr));
+
+        System.out.println(threeAdjInc(arr));
+    }
 }
