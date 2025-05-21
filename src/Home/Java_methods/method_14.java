b18b82c966849ce141c8dd7ad99c9c921ef774ba
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-21 13:59:33 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_methods/method_14.java
+++ src/Home/Java_methods/method_14.java
@@ -3,9 +3,19 @@ package Home.Java_methods;
 import java.util.Scanner;
 
 public class method_14 {
+//    Java method to create a pentagon's area.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input the number of sides:");
-        int n = sc
+        System.out.print("Input the side:");
+        double s = sc.nextDouble();
+        System.out.print("Input the number of sides:");
+        int  n = sc.nextInt();
+
+        double area = createArea(s, n);
+        System.out.println("The area of the pentagon is :"+area);
+
+    }
+    public static double createArea(double s , int n){
+        return (n*s*s)/(4*Math.tan(Math.PI/n));
     }
 }
