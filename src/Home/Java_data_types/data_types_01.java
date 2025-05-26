300db03f646c1dbb0ef934bb6c0c9d012f65da9d
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-26 11:29:13 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_data_types/data_types_01.java
+++ src/Home/Java_data_types/data_types_01.java
@@ -1,7 +1,18 @@
 package Home.Java_data_types;
 
+import java.util.Scanner;
+
 public class data_types_01 {
     public static void main(String[] args) {
+    //convert temperature from Fahrenheit to Celsius degrees.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a degree in Fahrenheit: ");
+        double Fahrenheit = sc.nextDouble();
+
+        double celsius = (Fahrenheit - 32) * 5 / 9;
+
+        System.out.println(Fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
 
     }
 }
