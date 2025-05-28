36f268e9ae4dd8914d279fe4beddaa41773a8ebf
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-28 14:01:02 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_data_types/data_types_12.java
+++ src/Home/Java_data_types/data_types_12.java
@@ -1,7 +1,20 @@
 package Home.Java_data_types;
 
+import java.util.Scanner;
+
 public class data_types_12 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Dividend: ");
+        int dividend = scanner.nextInt();
 
-    }
+        System.out.print("Enter Divisor: ");
+        int divisor = scanner.nextInt();
+
+        int floorDiv = Math.floorDiv(dividend, divisor);
+        int floorMod = Math.floorMod(dividend, divisor);
+
+        System.out.println("Floor Division of " + dividend + " / " + divisor + " = " + floorDiv);
+        System.out.println("Floor Modulus of " + dividend + " / " + divisor + " = " + floorMod);
+     }
 }
