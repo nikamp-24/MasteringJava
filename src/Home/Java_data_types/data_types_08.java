5056af93f038dcb799e6e624d31d9e71ab688c9a
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-28 12:03:20 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_data_types/data_types_08.java
+++ src/Home/Java_data_types/data_types_08.java
@@ -1,7 +1,23 @@
 package Home.Java_data_types;
 
+import java.util.Scanner;
+
 public class data_types_08 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input 1st integer: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Input 2st integer:");
+        int num2 = sc.nextInt();
+
+        System.out.printf("Sum of two integers: %d%n", num1 + num2);
+        System.out.printf("Difference of two integers: %d%n", num1 - num2);
+        System.out.printf("Product of two integers: %d%n", num1 * num2);
+        System.out.printf("Average of two integers: %.2f%n", (double) (num1 + num2 )/ 2);
+        System.out.printf("Distance of two integers: %d%n", Math.abs(num1-num2));
+        System.out.printf("Max integer: %d%n", Math.max(num1, num2));
+        System.out.printf("Min integer: %d%n", Math.min(num1, num2));
 
     }
 }
