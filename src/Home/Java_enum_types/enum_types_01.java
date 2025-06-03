e317aedf75c52a9a26207f626124684785c469c0
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-06-03 11:28:40 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_enum_types/enum_types_01.java
+++ src/Home/Java_enum_types/enum_types_01.java
@@ -1,26 +1,20 @@
 package Home.Java_enum_types;
 
-// Enum declared outside the class for better reusability and clarity
-enum DaysOfWeek {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
-}
-
 public class enum_types_01 {
 
+    public enum DaysOfWeek {
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY
+
+    }
+
     public static void main(String[] args) {
-
-        DaysOfWeek today = DaysOfWeek.MONDAY;
-
-
-        System.out.println("Today is: " + today);
-
-        System.out.println("\nAll Days of the Week:");
+        System.out.println("Days of the Week:");
         for (DaysOfWeek day : DaysOfWeek.values()) {
             System.out.println(day);
         }
--- src/Home/Java_enum_types/enum_types_03.java
+++ src/Home/Java_enum_types/enum_types_03.java
@@ -1,7 +1,29 @@
 package Home.Java_enum_types;
 
 public class enum_types_03 {
-    public static void main(String[] args) {
+    public enum Shape{
+        CIRCLE(0),
+        TRIANGLE(3),
+        SQUARE(4),
+        RECTANGLE(4),
+        PENTAGON(5),
+        HEXAGON(6);
 
+        private final int sides;
+
+
+        Shape(int sides){
+            this.sides = sides;
+        }
+
+        public int getSides() {
+            return sides;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Shapes and their number of sides:");
+        for (Shape shape : Shape.values()) {
+            System.out.println(shape + " has " + shape.getSides() + " sides.");
+        }
     }
 }
