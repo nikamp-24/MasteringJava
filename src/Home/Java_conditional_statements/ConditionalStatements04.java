3fc82c8e7d6b9fd78bf7ba4ba63ade7519bef6ce
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-06-11 18:18:56 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_conditional_statements/ConditionalStatements04.java
+++ src/Home/Java_conditional_statements/ConditionalStatements04.java
@@ -1,4 +1,27 @@
 package Home.Java_conditional_statements;
 
+import java.util.Scanner;
+
 public class ConditionalStatements04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        float num = sc.nextFloat();
+
+        if (num == 0) {
+            System.out.println(0);
+        } else if (num > 0) {
+            System.out.println("Positive number");
+            if (num > 0 && num < 1) {
+                System.out.println("small");
+            }
+            if (num > 1000000) {
+                System.out.println("large");
+            }
+        } else {
+            System.out.println("Negative number ");
+        }
+
+        sc.close();
+    }
 }
