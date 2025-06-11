34cff478d78f90783251468b0b13fc7a1b0a4c26
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-06-11 18:32:18 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_conditional_statements/ConditionalStatements05.java
+++ src/Home/Java_conditional_statements/ConditionalStatements05.java
@@ -1,4 +1,45 @@
 package Home.Java_conditional_statements;
 
+import java.util.Scanner;
+
 public class ConditionalStatements05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        getDayName(num);
+
+        sc.close();
+
+    }
+
+    public static void getDayName(int num) {
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+            default:
+                System.out.println("Invalid input. Please enter a number between 1 and 7.");
+
+        }
+
+
+    }
 }
