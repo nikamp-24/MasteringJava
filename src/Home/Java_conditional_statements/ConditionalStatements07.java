e72c5f8d88ff5e078c162df2cd5889754e00e35b
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-06-18 08:50:48 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/Home/Java_conditional_statements/ConditionalStatements07.java
@@ -0,0 +1,80 @@
+package Home.Java_conditional_statements;
+
+import java.util.Scanner;
+
+public class ConditionalStatements07 {
+    public static void main(String[] args) {
+//   Write a Java program to find the number of days in a month.
+        Scanner sc = new Scanner(System.in);
+
+        String MonthOfName = "";
+        int number_Of_DaysInMonth = 0 ;
+
+        System.out.print("Input a month number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Input a year: ");
+        int year = sc.nextInt();
+
+      switch (n){
+          case 1:
+              MonthOfName = "January";
+              number_Of_DaysInMonth = 31;
+              break;
+          case 2 :
+              MonthOfName = "February";
+              if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))){
+                  number_Of_DaysInMonth = 29;
+              }else {
+                  number_Of_DaysInMonth = 28;
+              }
+              break;
+          case 3 :
+              MonthOfName = "March";
+              number_Of_DaysInMonth = 31;
+              break;
+          case 4 :
+              MonthOfName = "April";
+              number_Of_DaysInMonth = 30;
+              break;
+          case 5 :
+              MonthOfName = "May";
+              number_Of_DaysInMonth = 31;
+              break;
+          case 6 :
+              MonthOfName = "June";
+              number_Of_DaysInMonth = 30;
+              break;
+          case  7:
+              MonthOfName = "July";
+              number_Of_DaysInMonth = 30;
+              break;
+          case 8:
+              MonthOfName = "August";
+              number_Of_DaysInMonth = 30;
+              break;
+          case 9 :
+              MonthOfName = "September";
+              number_Of_DaysInMonth = 30;
+              break;
+          case 10:
+              MonthOfName = "October";
+              number_Of_DaysInMonth = 30;
+              break;
+          case 11:
+              MonthOfName = "November";
+              number_Of_DaysInMonth = 30;
+              break;
+          case 12 :
+              MonthOfName = "December";
+              number_Of_DaysInMonth = 30;
+              break;
+          default:
+              System.out.println("Please input  valid month number");
+
+      }
+
+        System.out.println(MonthOfName + " " + year + " has " + number_Of_DaysInMonth + " days\n");
+
+    }
+}
