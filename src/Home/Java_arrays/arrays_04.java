91724a1270c8a11379b330e7bdf8df28fa81a1c6
Pooja <nikamp870@gmail.com>
2025-08-27 21:04:15 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_arrays/arrays_04.java
+++ src/Home/Java_arrays/arrays_04.java
@@ -1,7 +1,21 @@
 package Home.Java_arrays;
 
-public class arrays_04 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class arrays_04 {
+
+    //    reverse an array without using another array
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int temp;
+        int length = arr.length-1;
+        for (int i = 0; i <arr.length/2; i++){
+            temp = arr[i];
+            arr[i] = arr[length-i];
+            arr[length-i] = temp;
+
+        }
+
+        System.out.println("The reversed array is :" + Arrays.toString(arr));
     }
 }
