5f67a94a2e315c6a408b2541b2f3f857b11f6916
Pooja <nikamp870@gmail.com>
2025-09-02 14:39:54 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/Home/Java_patterns/Pattern_06.java
@@ -0,0 +1,12 @@
+package Home.Java_patterns;
+
+public class Pattern_06 {
+    public static void main(String[] args) {
+        for (int i =0; i<5; i++){
+            for (int j = 5-i; j>0; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
