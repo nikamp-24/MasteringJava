5786494e121f61ad2c911f0939c42ec3e4cb1621
Pooja <nikamp870@gmail.com>
2025-09-06 10:41:54 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_patterns/Pattern_19.java
+++ src/Home/Java_patterns/Pattern_19.java
@@ -21,7 +21,7 @@ public class Pattern_19 {
             spaces+=2;
             System.out.println();
         }
-        spaces = 8;
+        spaces = 2 *n -2;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
