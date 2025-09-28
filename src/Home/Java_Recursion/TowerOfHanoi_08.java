b64e1b49f1c8737230d81275b2cc34888353fd95
Pooja <nikamp870@gmail.com>
2025-09-28 09:36:43 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Home/Java_Recursion/TowerOfHanoi_08.java
+++ src/Home/Java_Recursion/TowerOfHanoi_08.java
@@ -1,4 +1,19 @@
 package Home.Java_Recursion;
 
 public class TowerOfHanoi_08 {
+    public static void main(String[] args) {
+        int n = 3;
+        solve(n, 'A', 'C', 'B');
+    }
+
+    static void solve(int n, char from, char to, char aux) {
+        if (n == 1) {
+            System.out.println("Move disk from " + from + " to " + to);
+            return;
+        }
+
+        solve(n-1, from , aux, to);
+        System.out.println("Move disk " + n + " from " + from + " to " + to);
+        solve(n-1, aux, to, from);
+    }
 }
